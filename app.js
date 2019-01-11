0edc8d1e61c3d140a27a0273db25366e9a5bd119
VelizarT <velizar_tonchev@abv.bg>
2019-01-11 15:45:24 +0000

    custom directive added

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.config(function($routeProvider) {
 
 app.service('cityService', function() {
 
-    this.city = 'Ex. London';
+    this.city = 'London, UK';
 
 });
 
@@ -40,10 +40,51 @@ app.controller('homeController', ['$scope', 'cityService', function($scope, city
 
 }]);
 
-app.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+app.controller('forecastController', ['$scope', '$http', 'cityService', function($scope, $http, cityService) {
 
     $scope.city = cityService.city;
 
+    $scope.formattedDate = function(datetime) {
+        return new Date(datetime);
+    };
+
+    $scope.toCelsius = function(temp) {
+        return Math.round(+temp - 272.15) + ' C';
+    };
+
+    // $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast', { q: $scope.city, APPID: '2027ae8eea532c45dab4a1954c46169c' });
+
+    // $scope.weatherResult = $scope.weatherAPI.get();
+
+    // $scope.weatherResult.then(function(response) {
+    //     $scope.weatherData = response.data.list;
+    // })
+
+    $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.city + '&APPID=2027ae8eea532c45dab4a1954c46169c')
+        .then(function successHandler(response) {
+
+            $scope.weatherData = response.data.list;
+
+        }, function errorHandler(response) {
+            alert('Unable to fetch data');
+        });
+
+
+
 }]);
+
+//Directives:
+
+app.directive('forecastItem', function() {
+    return {
+        templateUrl: 'directives/forecast-item.html',
+        replace: true,
+        scope: {
+            dailyForecastObject: '=',
+            formattedDate: '&',
+            toCelsius: '&'
+        }
+    }
+});
 
 
